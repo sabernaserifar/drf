5750feb5592b5b9a9e5251911110e4a153440298
sabernaserifar <naserifar.saber@gmail.com>
2022-12-20 15:15:13 -0800

    Add customize permission module and update views for that

--- api/views.py
+++ api/views.py
@@ -2,12 +2,13 @@ from rest_framework import generics, permissions
 from .serializers import PurchaseSerializer, UserSerializer
 from django.contrib.auth.models import User
 from .models import Purchase
+from .permissions import IsOwnerOrReadOnly
 
 
 class PurchaseList(generics.ListCreateAPIView):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -16,7 +17,7 @@ class PurchaseList(generics.ListCreateAPIView):
 class PurchaseDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
