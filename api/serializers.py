d7425b9804dc3d2fb3f254acae040b2e1337803a
sabernaserifar <naserifar.saber@gmail.com>
2022-12-20 15:42:00 -0800

    Add hyperlink for relations

--- api/serializers.py
+++ api/serializers.py
@@ -3,15 +3,15 @@ from .models import Purchase
 from django.contrib.auth.models import User
 
 
-class PurchaseSerializer(serializers.ModelSerializer):
+class PurchaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Purchase
         fields = '__all__'
 
 
-class UserSerializer(serializers.ModelSerializer):
-    purchases = serializers.PrimaryKeyRelatedField(many=True, queryset=Purchase.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    purchases = serializers.HyperlinkedRelatedField(many=True, view_name='purchase-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'purchases']
+        fields = ['url', 'id', 'username', 'purchases']
