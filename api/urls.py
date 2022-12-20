9cbc6ff967c76c5e9a4e4796220ee41c56318842
sabernaserifar <naserifar.saber@gmail.com>
2022-12-20 15:42:37 -0800

    Add home page view for rest framework

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('purchases/', views.PurchaseList.as_view()),
-    path('purchases/<int:pk>/', views.PurchaseDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('purchases/', views.PurchaseList.as_view(), name='purchase-list'),
+    path('purchases/<int:pk>/', views.PurchaseDetail.as_view(), name='purchase-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root)
 ]
 
 
--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics, permissions
 from .serializers import PurchaseSerializer, UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 from .models import Purchase
 from .permissions import IsOwnerOrReadOnly
@@ -29,3 +32,10 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'purchases': reverse('purchase-list', request=request, format=format)
+    })
